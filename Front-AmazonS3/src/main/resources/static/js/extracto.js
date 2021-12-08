5117175f47a98b7eabf32bc9ca6460667ed89975
lauramilena.ramosbermudez.contractor <lauramilena.ramosbermudez.contractor@gdevtools.com>
2021-12-07 20:05:09 -0500

    Access bucket jhordy

--- Front-AmazonS3/src/main/resources/static/js/extracto.js
+++ Front-AmazonS3/src/main/resources/static/js/extracto.js
@@ -4,17 +4,16 @@ $(document).ready(function(){
 var extracto=(function () {
     var map = new Map();
     var s3 ;
-    const myBucket = 'demo-rappipay-s3'
+    const myBucket = 'aygo-prototype'
 
     function base64ToArrayBuffer(keyPDF) {
         var bucketParams = {
             Bucket : myBucket,
             Key: keyPDF
         };
-
         var promise = s3.getSignedUrlPromise('getObject', bucketParams);
         promise.then(function(url) {
-           window.open(url,'', 'height=650,width=840');
+           window.open(url,'', 'height=700,width=840');
         }, function(err) {
             alert("Error", err);
         });
@@ -36,24 +35,26 @@ var extracto=(function () {
 
     function getextracto(){
         var contract = document.getElementById('contractid').value
+        var statement= "statements/";
         if(contract===""){
             alert("contrato invalido");
         }
         else{
-            AWS.config.update({accessKeyId: 'AKIAVFHJMDPBUAIK7CEO',
-                                secretAccessKey: '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',
+            AWS.config.update({accessKeyId: 'AKIAUGETR7TEV77ELSJT',//Lau: AKIAVFHJMDPBUAIK7CEO
+                                secretAccessKey: '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',//Lau: zt9D0XyQ2sQWCpRezhvLnwqNAbUOSf1KGh8vziDG
                                 region: 'us-east-1'
             })
             s3 = new AWS.S3()
             var bucketParams = {
                 Bucket : myBucket,
-                Prefix: contract.trim().concat('/')
+                Prefix: statement.concat(contract.trim().concat('/'))
             };
             s3.listObjectsV2(bucketParams, function(err, data) {
               if (err) {
                 alert("Error", err);
               } else {
                 $(".rowTable").remove();
+                console.log(data.Contents)
                 if(data.Contents.length>1){
                     successS3Object(data.Contents,contract).map(addRow);
                 }
